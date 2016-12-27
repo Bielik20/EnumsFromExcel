6d2541bc7ca209a3bb58401d4cf40921063256c7
Damian Bielecki <damian.x.bielecki@gsk.com>
2016-12-27 18:24:31 +0100

    Added SampleFiles

---
+++ SampleFiles/BaseFiles/DatabaseBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    public class FILL_IN_TITLE
+    {
+        [ForeignKey("ParentElement")]
+        [Key, Column(Order = 0)]
+        public int ParentElementId { get; set; }
+        [Key, Column(Order = 1)]
+        public FILL_IN_ENUM_TITLE Value { get; set; }
+
+        public virtual ParentElement ParentElement { get; set; }
+    }
+}
