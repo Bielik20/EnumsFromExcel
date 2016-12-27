a2e975b6d9a71c08c500259934bbdd10be1dfe24
Damian Bielecki <damian.x.bielecki@gsk.com>
2016-12-27 17:46:09 +0100

    Updated EnumModel

--- EnumsFromExcel/EnumModel.cs
+++ EnumsFromExcel/EnumModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,5 +11,29 @@ namespace EnumsFromExcel
     {
         public string Title { get; set; }
         public List<string> EnumsList { get; set; } = new List<string>();
+
+        public string GetFormatedTitle()
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in textInfo.ToTitleCase(Title))
+            {
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_')
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        public string GetFormatedEnumsList()
+        {
+            string list = "";
+            for (int i = 0; i < EnumsList.Count; i++)
+            {
+                list += $"\t\t[Display(Name = \"{EnumsList[i]}\")]\n" + $"\t\tA{i},\n";
+            }
+            return list;
+        }
     }
 }
