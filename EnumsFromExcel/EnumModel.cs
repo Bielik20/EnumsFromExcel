32a1d39ee27e4bcc860732ed053f2a0b270f053b
Damian Bielecki <damian.x.bielecki@gsk.com>
2016-12-27 14:44:20 +0100

    Added EnumModel

---
+++ EnumsFromExcel/EnumModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsFromExcel
+{
+    class EnumModel
+    {
+        public string Title { get; set; }
+        public List<string> EnumsList { get; set; } = new List<string>();
+    }
+}
