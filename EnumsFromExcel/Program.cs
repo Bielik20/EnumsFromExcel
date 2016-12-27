d4ea0f4988ee438d330c41f952d8e5c22fc327ed
Damian Bielecki <damian.x.bielecki@gsk.com>
2016-12-27 18:17:13 +0100

    Added main program

--- EnumsFromExcel/Program.cs
+++ EnumsFromExcel/Program.cs
@@ -10,6 +10,12 @@ namespace EnumsFromExcel
     {
         static void Main(string[] args)
         {
+            var excelReader = new ExcelReader(System.AppDomain.CurrentDomain.BaseDirectory + @"\input.xlsx");
+            var enumModelList = excelReader.GetModelList();
+            var csWriter = new CsWriter(enumModelList);
+            csWriter.CreateFiles();
+            Console.WriteLine("Ready");
+            Console.ReadLine();
         }
     }
 }
